2d4a22ef27718f4bf653dfec0da7e9a342e20da6
Madalievnkx <146520663+Madalievnkx@users.noreply.github.com>
2023-12-09 20:17:04 +0100

    update file

---
+++ src/external/hlsdk/engine/wrect.h
@@ -0,0 +1,16 @@
+//========= Copyright © 1996-2002, Valve LLC, All rights reserved. ============
+//
+// Purpose: 
+//
+// $NoKeywords: $
+//=============================================================================
+
+#if !defined( WRECTH )
+#define WRECTH
+
+struct wrect_t
+{
+	int				left, right, top, bottom;
+};
+
+#endif
